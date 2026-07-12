9ad9977048158bd8adea2d41c38d275edbeeca8f
Rohit533-max <rohit03383@gmail.com>
2026-07-12 15:44:51 +0530

    adding files

---
+++ Weather.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import dotenv_values,load_dotenv
+import requests
+
+load_dotenv()
+key = os.getenv("Weather_key")
+
+
+def check_weather(city):
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    p = {"q": city, "appid": key, "units": "metric"}
+    try:
+        r = requests.get(url,params=p)
+        r.raise_for_status()
+        data = r.json()
+        print(f"{city}: {data['main']['temp']}°C {data['weather'][0]['description']}")
+        
+
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+check_weather("hamirpur")
